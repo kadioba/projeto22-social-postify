3e702924088c49cd14936604a476fbcb5a0f573a
Vinicius Costa Predebon <kadiobaoff@gmail.com>
2023-08-22 00:43:55 -0300

    feat: initial boilerplate

---
+++ src/medias/entities/media.entity.ts
@@ -0,0 +1,11 @@
+export class Media {
+  private id: number;
+  private title: string;
+  private username: string;
+
+  constructor(id: number, title: string, username: string) {
+    this.id = id;
+    this.title = title;
+    this.username = username;
+  }
+}
